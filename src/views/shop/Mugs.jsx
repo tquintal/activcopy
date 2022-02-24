04898fa25a4c2b6aae5b209c899386a4743d694e
Tomás Quintal <tomas.quintal@gmail.com>
2022-02-24 17:58:40 +0000

    mugs and stickers created - shop

--- src/views/shop/Mugs.jsx
+++ src/views/shop/Mugs.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-// import { useTranslation } from 'react-i18next'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { motion } from 'framer-motion'
 import ShopBack from '../../components/ShopBack'
+import BCards from '../../assets/b-cards.jpg'
 
 export default function Mugs() {
 
-    // const { t } = useTranslation()
+    const [order, setOrder] = useState({
+        Format: '90mm x 210mm',
+        Material: 'Caneca branca',
+        Printing: 'Cores',
+        Amount: '1',
+        Name: '',
+        EMail: '',
+        Contact: '',
+        Address: '',
+        File: false,
+        Note: '',
+        Total: '0€'
+    })
+
+    const LogOrder = () => {
+        console.table(order)
+    }
+
+    const { t } = useTranslation()
+
+    const setOrderCompleted = () => {
+        if (order.Name === '' || order.EMail === '' || order.Contact === '' || order.Address === '' || order.File !== true) {
+            LogOrder()
+            alert(t('Shop.Error'))
+        } else {
+            LogOrder()
+            localStorage.removeItem(['Order'])
+            console.log(`Local storage cleared`)
+            localStorage['Order'] = JSON.stringify(order)
+            console.log(`Order placed`)
+        }
+    }
 
     return (
         <motion.div
@@ -16,8 +48,69 @@ export default function Mugs() {
         >
             <div className='service'>
                 <div className='service-container'>
-                    <ShopBack />
-                    <h1>Mugs</h1>
+                    <div className='shop-cat-container'>
+                        <div className='shop-cat-left-side'>
+                            <ShopBack />
+                            <h1>{t('Shop.Mugs')}</h1>
+                            <div className='shop-cat-img'>
+                                <img src={BCards} alt='b-cards' onClick={LogOrder} />
+                            </div>
+                        </div>
+                        <div className='shop-cat-form-container'>
+                            <form method='POST' action='https://formsubmit.co/tomas.quintal@gmail.com' encType='multipart/form-data' className='shop-form'>
+                                <p>{t('ShopBusinessCards.Format')}</p>
+                                <select type='select' name='Formato' onChange={(e) => { setOrder({ ...order, Format: e.target.value }) }} required>
+                                    <option value='90mm x 210mm'>90mm x 210mm</option>
+                                </select>
+
+                                <p>{t('ShopBusinessCards.Material')}</p>
+                                <select type='select' name='Material' onChange={(e) => { setOrder({ ...order, Material: e.target.value }) }} required>
+                                    <option value='Caneca branca'>Caneca branca</option>
+                                    <option value='Caneca mágica preta'>Caneca mágica preta</option>
+                                    <option value='Caneca fluorescente amarela'>Caneca fluorescente amarela</option>
+                                    <option value='Caneca fluorescente laranja'>Caneca fluorescente laranja</option>
+                                    <option value='Caneca fluorescente verde'>Caneca fluorescente verde</option>
+                                    <option value='Caneca fluorescente vermelha'>Caneca fluorescente vermelha</option>
+                                </select>
+
+                                <p>{t('ShopBusinessCards.Printing')}</p>
+                                <select type='select' name='Impressao' onChange={(e) => { setOrder({ ...order, PrintingColor: e.target.value }) }} required>
+                                    <option value='Cores'>{t('ShopBusinessCards.FirstPrinting2')}</option>
+                                    <option value='Preto e branco'>{t('ShopBusinessCards.SecondPrinting2')}</option>
+                                </select>
+
+                                <p>{t('ShopBusinessCards.Amount')} *</p>
+                                <input type='text' name='Quantidade' placeholder='Quantidade *' onChange={(e) => { setOrder({ ...order, Amount: e.target.value }) }} required></input>
+
+                                <input type='text' name='Nome' placeholder='Nome *' onChange={(e) => { setOrder({ ...order, Name: e.target.value }) }} required></input>
+                                <input type='email' name='E-Mail' placeholder='E-mail *' onChange={(e) => { setOrder({ ...order, EMail: e.target.value }) }} required></input>
+                                <input type='text' name='Contacto' placeholder={t('ShopBusinessCards.Contact') + ' *'} onChange={(e) => { setOrder({ ...order, Contact: e.target.value }) }} required></input>
+                                <input type='text' name='Morada' placeholder={t('ShopBusinessCards.Address') + ' *'} onChange={(e) => { setOrder({ ...order, Address: e.target.value }) }} required></input>
+                                <label>
+                                    {t('ShopBusinessCards.UploadFile') + ' *'}
+                                    <input type='file' name='Attachment' accept='image/png, image/jpeg' onChange={(e) => { setOrder({ ...order, File: true }) }} className='shop-attachment' required></input>
+                                </label>
+                                <textarea name='Comentario' placeholder={t('ShopBusinessCards.Note')} onChange={(e) => { setOrder({ ...order, Note: e.target.value }) }} className='shop-text-area' />
+                                <p>Total: {order.Total}</p>
+
+                                {/* USER INFO */}
+                                <input type='hidden' name='_cc' value={order.EMail}></input>
+                                <input type='hidden' name='IBAN' value={'XXX XXX XXX XXX XXX'}></input>
+                                <input type='hidden' name='OUTRO(?)' value={'XXX XXX XXX XXX XXX'}></input>
+                                <input type='hidden' name='Valor' value={order.Total}></input>
+
+                                <input type='hidden' name='_captcha' value='false'></input>
+                                <p className='shop-required-fields'>{t('Form.Info')}</p>
+
+                                <input type='hidden' name='_template' value='table'></input>
+
+                                <button type='submit' onClick={setOrderCompleted} className='shop-button'>{t('ShopBusinessCards.Order')}</button>
+                                {/* <input type='hidden' name='_next' value='https://activcopy.vercel.app/' /> */}
+                                <input type='hidden' name='_next' value='https://activcopy.vercel.app/shop/order-completed' />
+                                {/* <input type='hidden' name='_next' value='http://localhost:3000/shop/order-completed' /> */}
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </motion.div>
