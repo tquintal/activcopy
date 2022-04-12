6322fb91b4b3669b443d9dd7d176dcbe51b3d1aa
Tomás Quintal <tomas.quintal@gmail.com>
2022-04-12 19:14:03 +0100

    shop > flyers done

--- src/views/shop/Flyers.jsx
+++ src/views/shop/Flyers.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { roundUp } from '../../utils'
 import { motion } from 'framer-motion'
 import ShopBack from '../../components/ShopBack'
 import BCards from '../../assets/b-cards.jpg'
@@ -21,7 +22,7 @@ export default function Flyers() {
         PromoCode: '',
         File: false,
         Note: '',
-        Total: '0€'
+        Total: '20€'
     })
 
     const LogOrder = () => {
@@ -30,11 +31,96 @@ export default function Flyers() {
 
     const { t } = useTranslation()
 
+    const [total, setTotal] = useState(20)
+
+    let cTotal = 20
+
+    const calculate = (format, material, printing, quantity, finish) => {
+        if (format === 'A6') {
+            if (material === 'Tecno Color 100g' || material === 'Couché 135g') {
+                if (printing === 'Frente') {
+                    cTotal = 6
+                } else {
+                    cTotal = 12
+                }
+            } else {
+                if (printing === 'Frente') {
+                    cTotal = 7.25
+                } else {
+                    cTotal = 13.25
+                }
+            }
+        }
+
+        if (format === 'A5') {
+            if (material === 'Tecno Color 100g' || material === 'Couché 135g') {
+                if (printing === 'Frente') {
+                    cTotal = 10
+                } else {
+                    cTotal = 20
+                }
+            } else {
+                if (printing === 'Frente') {
+                    cTotal = 13
+                } else {
+                    cTotal = 23
+                }
+            }
+        }
+
+        if (format === 'A4') {
+            if (material === 'Tecno Color 100g' || material === 'Couché 135g') {
+                if (printing === 'Frente') {
+                    cTotal = 20
+                } else {
+                    cTotal = 30
+                }
+            } else {
+                if (printing === 'Frente') {
+                    cTotal = 25
+                } else {
+                    cTotal = 35
+                }
+            }
+        }
+
+        switch (quantity) {
+            case '200':
+                cTotal = cTotal * 1.540
+                break
+            case '500':
+                cTotal = cTotal + cTotal * 1.661
+                break
+            case '1000':
+                cTotal = cTotal + cTotal * 1.800
+                break
+            case '2000':
+                cTotal = cTotal + cTotal * 1.831
+                break
+            case '3000':
+                cTotal = cTotal + cTotal * 1.439
+                break
+            default:
+                cTotal = cTotal + 0
+        }
+
+        if (finish !== '') {
+            cTotal = cTotal + quantity * 0.05
+        }
+
+        if (order.PromoCode === 'activ10') {
+            cTotal = cTotal * 0.9
+        }
+
+        return roundUp(cTotal, 2)
+    }
+
     const setOrderCompleted = () => {
         if (order.Name === '' || order.EMail === '' || order.Contact === '' || order.Address === '' || order.File !== true) {
             LogOrder()
             alert(t('Shop.Error'))
         } else {
+            order.Total = `${total}€`
             LogOrder()
             localStorage.removeItem(['Order'])
             console.log(`Local storage cleared`)
@@ -71,21 +157,30 @@ export default function Flyers() {
                                 <input type='hidden' name='Encomenda' value='Flyers'></input>
 
                                 <p>Formato</p>
-                                <select type='select' name='Formato' onChange={(e) => { setOrder({ ...order, Format: e.target.value }) }} required>
+                                <select type='select' name='Formato' onChange={(e) => {
+                                    setOrder({ ...order, Format: e.target.value })
+                                    setTotal(calculate(e.target.value, order.Material, order.Printing, order.Amount, order.Finish))
+                                }} required>
                                     <option value='A4'>A4</option>
                                     <option value='A5'>A5</option>
                                     <option value='A6'>A6</option>
                                 </select>
 
                                 <p>Material</p>
-                                <select type='select' name='Material' onChange={(e) => { setOrder({ ...order, Material: e.target.value }) }} required>
+                                <select type='select' name='Material' onChange={(e) => {
+                                    setOrder({ ...order, Material: e.target.value })
+                                    setTotal(calculate(order.Format, e.target.value, order.Printing, order.Amount, order.Finish))
+                                }} required>
                                     <option value='Tecno Color 100g'>Tecno Color 100g</option>
                                     <option value='Couché 135g'>Couché 135g</option>
                                     <option value='Papel reciclado 100g'>Papel reciclado 100g</option>
                                 </select>
 
                                 <p>Impressão</p>
-                                <select type='select' name='Impressão' onChange={(e) => { setOrder({ ...order, Printing: e.target.value }) }} required>
+                                <select type='select' name='Impressão' onChange={(e) => {
+                                    setOrder({ ...order, Printing: e.target.value })
+                                    setTotal(calculate(order.Format, order.Material, e.target.value, order.Amount, order.Finish))
+                                }} required>
                                     <option value='Frente'>Frente</option>
                                     <option value='Frente e verso'>Frente e verso</option>
                                 </select>
@@ -95,7 +190,10 @@ export default function Flyers() {
                                 </select>
 
                                 <p>Quantidade</p>
-                                <select type='select' name='Quantidade' onChange={(e) => { setOrder({ ...order, Amount: e.target.value }) }} required>
+                                <select type='select' name='Quantidade' onChange={(e) => {
+                                    setOrder({ ...order, Amount: e.target.value })
+                                    setTotal(calculate(order.Format, order.Material, order.Printing, e.target.value, order.Finish))
+                                }} required>
                                     <option value='100'>100</option>
                                     <option value='200'>200</option>
                                     <option value='500'>500</option>
@@ -109,6 +207,7 @@ export default function Flyers() {
                                     <input type='radio' name='Acabamento' value='Dobra em U' onChange={() => {
                                         setFinishSelected('Dobra em U')
                                         setOrder({ ...order, Finish: 'Dobra em U' })
+                                        setTotal(calculate(order.Format, order.Material, order.Printing, order.Amount, 'Dobra em U'))
                                     }} checked={finishSelected === 'Dobra em U'} />
                                     Dobra em U
                                 </label>
@@ -116,6 +215,7 @@ export default function Flyers() {
                                     <input type='radio' name='Acabamento' value='Dobra em Z' onChange={() => {
                                         setFinishSelected('Dobra em Z')
                                         setOrder({ ...order, Finish: 'Dobra em Z' })
+                                        setTotal(calculate(order.Format, order.Material, order.Printing, order.Amount, 'Dobra em Z'))
                                     }} checked={finishSelected === 'Dobra em Z'} />
                                     Dobra em Z
                                 </label>
@@ -133,17 +233,21 @@ export default function Flyers() {
                                 <div className='shop-promo-code'>
                                     <input type='text' name='Promoção' placeholder='Código promocional' onChange={(e) => { setOrder({ ...order, PromoCode: e.target.value.toLowerCase() }) }}></input>
                                     <div onClick={() => {
-                                        order.PromoCode === 'activ10' ?
-                                            alert('Correct Code')
-                                            : alert('Error')
+                                        if (order.PromoCode === 'activ10') {
+                                            setTotal(calculate(order.Format, order.Material, order.Printing, order.Amount, order.Finish))
+                                            alert('Cupão aplicado com sucesso!')
+                                        }
+                                        else {
+                                            alert('Error')
+                                        }
                                     }}>Aplicar</div>
                                 </div>
-                                <p>Total: {order.Total}</p>
+                                <p>Total: {total}€</p>
 
                                 {/* USER INFO */}
                                 <input type='hidden' name='_cc' value={order.EMail}></input>
                                 <input type='hidden' name='NIB' value='PT50 0033 0000 4534 1788 5440 5'></input>
-                                <input type='hidden' name='Valor' value={order.Total}></input>
+                                <input type='hidden' name='Valor' value={`${total}€`}></input>
 
                                 <p className='shop-required-fields'>IVA incluído</p>
 
